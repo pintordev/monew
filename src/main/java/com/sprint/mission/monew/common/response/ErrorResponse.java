51ddf66c2c0b93a590ef9bc2949a1444decd3a55
pintor.dev <pintor.dev@gmail.com>
2026-05-22 01:27:56 +0900

    feat: ErrorResponse 공통 에러 응답 레코드 구현

---
+++ src/main/java/com/sprint/mission/monew/common/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.monew.common.response;
+
+import java.time.Instant;
+import java.util.Map;
+
+public record ErrorResponse(
+    Instant timestamp,
+    String code,
+    String message,
+    Map<String, Object> details,
+    String exceptionType,
+    int status
+) {}

9e3a5974df74ea4138f519b9bd1b4c5227e47b16
pintor.dev <pintor.dev@gmail.com>
2026-05-26 16:05:08 +0900

    test(green): InterestRepository 저장 후 조회 구현

---
+++ src/main/java/com/sprint/mission/monew/domain/interest/repository/InterestRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.monew.domain.interest.repository;
+
+import com.sprint.mission.monew.domain.interest.entity.Interest;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, UUID> {
+
+}

13b8b6c9f58e3b198cc074dc3cea339ccc531b14
pintor.dev <pintor.dev@gmail.com>
2026-05-26 17:36:07 +0900

    fix: InterestCreateRequest keywords에 @NotNull 추가

--- src/main/java/com/sprint/mission/monew/domain/interest/dto/InterestCreateRequest.java
+++ src/main/java/com/sprint/mission/monew/domain/interest/dto/InterestCreateRequest.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.monew.domain.interest.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record InterestCreateRequest(
-    @NotBlank @Size(max = 50) String name, @Size(min = 1, max = 10) List<String> keywords) {}
+    @NotBlank @Size(max = 50) String name,
+    @NotNull @Size(min = 1, max = 10) List<@NotBlank String> keywords) {}

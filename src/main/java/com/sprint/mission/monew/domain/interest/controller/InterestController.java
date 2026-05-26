f6d4ea0eb76b248e6cabbe5a37f97dd5424dda6e
pintor.dev <pintor.dev@gmail.com>
2026-05-26 17:09:51 +0900

    test(green): InterestController 구현으로 관심사 등록 컨트롤러 테스트 통과

---
+++ src/main/java/com/sprint/mission/monew/domain/interest/controller/InterestController.java
@@ -0,0 +1,33 @@
+package com.sprint.mission.monew.domain.interest.controller;
+
+import com.sprint.mission.monew.domain.interest.controller.api.InterestApi;
+import com.sprint.mission.monew.domain.interest.dto.InterestCreateRequest;
+import com.sprint.mission.monew.domain.interest.dto.InterestDto;
+import com.sprint.mission.monew.domain.interest.service.InterestService;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/interests")
+@RequiredArgsConstructor
+public class InterestController implements InterestApi {
+
+  private final InterestService interestService;
+
+  @PostMapping
+  @Override
+  public ResponseEntity<InterestDto> create(
+      @Valid @RequestBody InterestCreateRequest request,
+      @RequestHeader("Monew-Request-User-ID") UUID requestUserId
+  ) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(interestService.create(request, requestUserId));
+  }
+}
---
+++ src/main/java/com/sprint/mission/monew/domain/interest/controller/api/InterestApi.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.monew.domain.interest.controller.api;
+
+import com.sprint.mission.monew.domain.interest.dto.InterestCreateRequest;
+import com.sprint.mission.monew.domain.interest.dto.InterestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@Tag(name = "Interest", description = "관심사 API")
+public interface InterestApi {
+
+  @Operation(summary = "관심사 등록")
+  ResponseEntity<InterestDto> create(
+      @Valid @RequestBody InterestCreateRequest request,
+      @RequestHeader("Monew-Request-User-ID") UUID requestUserId
+  );
+}

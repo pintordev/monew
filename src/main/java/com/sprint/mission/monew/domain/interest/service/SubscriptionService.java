a32860443d4e7a6f6f08b7030737699a39ea9d42
pintor.dev <pintor.dev@gmail.com>
2026-05-28 15:40:48 +0900

    test(green): 존재하지 않는 관심사 구독 시 InterestNotFoundException 발생 구현

---
+++ src/main/java/com/sprint/mission/monew/domain/interest/service/SubscriptionService.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.monew.domain.interest.service;
+
+import com.sprint.mission.monew.domain.interest.exception.InterestNotFoundException;
+import com.sprint.mission.monew.domain.interest.repository.InterestRepository;
+import com.sprint.mission.monew.domain.interest.repository.SubscriptionRepository;
+import com.sprint.mission.monew.domain.user.repository.UserRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SubscriptionService {
+
+  private final InterestRepository interestRepository;
+  private final UserRepository userRepository;
+  private final SubscriptionRepository subscriptionRepository;
+
+  @Transactional
+  public Object subscribe(UUID interestId, UUID userId) {
+    interestRepository.findById(interestId)
+        .orElseThrow(() -> InterestNotFoundException.withId(interestId));
+    return null;
+  }
+}

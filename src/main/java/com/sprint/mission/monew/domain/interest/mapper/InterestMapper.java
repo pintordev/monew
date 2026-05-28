f70972d11d85b96ef1d2d82b96cd2f061be5258d
pintor.dev <pintor.dev@gmail.com>
2026-05-28 12:06:42 +0900

    refactor: InterestMapper Collectors 완전경로 참조 제거

--- src/main/java/com/sprint/mission/monew/domain/interest/mapper/InterestMapper.java
+++ src/main/java/com/sprint/mission/monew/domain/interest/mapper/InterestMapper.java
@@ -2,13 +2,14 @@ package com.sprint.mission.monew.domain.interest.mapper;
 
 import com.sprint.mission.monew.domain.interest.dto.InterestResponse;
 import com.sprint.mission.monew.domain.interest.entity.Interest;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = Collectors.class)
 public interface InterestMapper {
 
   @Mapping(target = "subscribedByMe", constant = "false")
-  @Mapping(target = "keywords", expression = "java(interest.getKeywords().stream().map(k -> k.getKeyword()).collect(java.util.stream.Collectors.toList()))")
+  @Mapping(target = "keywords", expression = "java(interest.getKeywords().stream().map(k -> k.getKeyword()).collect(Collectors.toList()))")
   InterestResponse toResponse(Interest interest);
 }

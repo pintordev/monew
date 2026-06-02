30fec5358c72bd12d36c341c4ca608f471e3a176
pintor.dev <pintor.dev@gmail.com>
2026-06-02 22:10:50 +0900

    chore: .gitkeep 제거

--- src/main/java/com/sprint/mission/monew/external/naver/dto/NaverNewsItem.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.monew.external.naver.dto;
-
-public record NaverNewsItem(
-    String title,
-    String originallink,
-    String link,
-    String description,
-    String pubDate
-) {}

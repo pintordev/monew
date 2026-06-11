de7e24ddcfb4877f46b608395d2b00b8e517e632
pintor.dev <pintor.dev@gmail.com>
2026-06-11 19:23:30 +0900

    chore: 사용되지 않는 StopwordConfig, StopwordProperties 제거

--- src/main/java/com/sprint/mission/monew/common/config/StopwordConfig.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.mission.monew.common.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(StopwordProperties.class)
-public class StopwordConfig {}
--- src/main/java/com/sprint/mission/monew/common/config/StopwordProperties.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.monew.common.config;
-
-import java.util.List;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "monew.stopwords")
-public record StopwordProperties(List<String> suffix, List<String> prefix) {}
